c05211614877f7f347c024e0c9b0093941d9afe7
Valentin Lemaire <valentinlemaire337@gmail.com>
2023-04-27 10:50:31 +0200

    update rss

--- controllers/parameters-controller.php
+++ controllers/parameters-controller.php
@@ -1,13 +1,18 @@
 <?php
 require_once __DIR__ . '/../helpers/dd.php';
 
-$subjects = ['Actualités' => 'https://www.01net.com/actualites/feed/','Sécurité' => 'https://www.01net.com/actualites/securite/feed/','Football' =>'https://rmcsport.bfmtv.com/rss/football/','Actualités Jeux Vidéo' => 'https://www.jeuxactu.com/rss/ja.rss', 'Test Jeux Vidéo' =>'https://www.jeuxactu.com/rss/tests.rss'];
+$subjects = ['Tests' => 'https://www.01net.com/tests/feed/','Sécurité' => 'https://www.01net.com/actualites/securite/feed/','Football' =>'https://rmcsport.bfmtv.com/rss/football/','Actualités Jeux Vidéo' => 'https://www.jeuxactu.com/rss/ja.rss', 'Test Jeux Vidéo' =>'https://www.jeuxactu.com/rss/tests.rss'];
 
 // var_dump($subjects);
 
-$xml =  simplexml_load_file($subjects['Actualités']);
+$xml =  simplexml_load_file($subjects['Sécurité']);
 
-dd($xml);
+$subjectsTitle = $xml->channel->item->title;
+foreach ($subjectsTitle as $subjectTitle) {
+    d($subjectTitle);
+}
+
+// d($xml->channel);
 
 // démo thierry comment naviguer dans un xml
 
