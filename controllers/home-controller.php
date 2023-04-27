5e948ac098f1f279c9e7002cd5309289d2879c63
LaurenceB28 <laurence.brd@gmail.com>
2023-04-27 14:59:11 +0200

    articles

--- controllers/home-controller.php
+++ controllers/home-controller.php
@@ -5,4 +5,5 @@
 
 include __DIR__ . '/../views/header.php';
 include __DIR__ . '/../views/users/userPref.php';
+include __DIR__ . '/../views/users/articles.php';
 include __DIR__ . '/../views/footer.php';
