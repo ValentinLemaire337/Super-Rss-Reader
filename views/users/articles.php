b997dbff69d98a972ab4c97ffb4fd718ae443aa9
Valentin Lemaire <valentinlemaire337@gmail.com>
2023-04-28 17:01:26 +0200

    oui

--- views/users/articles.php
+++ views/users/articles.php
@@ -21,10 +21,8 @@ require __DIR__ . '/../../controllers/pages-controller.php';
                     <div class="card-body">
                         <h5 class="card-title">TITRE</h5>
                         <h6><?= $dateCard ?></h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn">En savoir plus</a>
-                        <h5 class="card-title">'. $articleTitle. '</h5>
                         <h6><?= $articleDate ?></h6>
+                        <h5 class="card-title">'. $articleTitle. '</h5>
                         <p class="card-text">'. $articleDesc .'</p>
                         <a href="'. $articleLink .'" class="btn">+</a>
                     </div>
