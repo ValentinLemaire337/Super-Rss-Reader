21766ebba70038bd1ce981dafaf724c1817a48bd
Valentin Lemaire <valentinlemaire337@gmail.com>
2023-05-02 14:29:14 +0200

    add view

---
+++ controllers/subjects_controller.php
@@ -0,0 +1,10 @@
+<?php 
+
+require_once __DIR__ . '/../helpers/dd.php';
+require __DIR__ . '/../config/default.php';
+require __DIR__ . '/data.php';
+
+
+include __DIR__ . '/../views/header.php';
+include __DIR__ . '/../views/users/subjects.php';
+include __DIR__ . '/../views/footer.php';
